4ec138cf4ff6653ad4bb8dcfe5f15733faaf2a18
chaewon <chaewoni21@gmail.com>
2023-07-15 11:21:52 +0900

    계정확인

--- src/main/java/woni/class1/app/Main.java
+++ src/main/java/woni/class1/app/Main.java
@@ -1,18 +1,13 @@
 package woni.class1.app;
 
-import woni.class1.app.calculator.Calculator;
-import woni.class1.app.calculator.Input;
+
+import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) {
-
-        Calculator calculator = new Calculator();
-        System.out.println("계산기 시작");
-        int result = calculator.plus();
-        System.out.println("===result===");
-        System.out.println(result);
-
+       
     }
 
 }
